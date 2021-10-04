b8be8f0143b5f45217a3940179d78baafc2214ea
Jean-Marie BERNARD <jm11bernard@gmail.com>
2021-10-04 17:07:59 +0200

    Routing players OK

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const usersRouter = require('./users.routes');
 const adminRouter = require('./admin.routes');
-// const playersRouter = require('./players.routes');
+const playersRouter = require('./players.routes');
 // const membersRouter = require('./members.routes');
 
 router.use('/users', usersRouter);
 router.use('/admin', adminRouter);
-// router.use('/players', playersRouter);
+router.use('/players', playersRouter);
 // router.use('/members', membersRouter);
 
 module.exports = router;
